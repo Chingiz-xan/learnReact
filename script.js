f2b4c0712f069f077cee4df27b00b3b608895d8f
Chingiz <cingiz99@mail.ru>
2021-04-21 21:47:07 +0400

    Prompt

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-"use strict";
+const numberOfFilms = +prompt("How much film?");
 
-console.log('arr' + " - object");
+    const personalMovieDB = {
+        count:numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+    const a = prompt("Last your film",""),
+          b = prompt("Rating",""),
+          c = prompt("Last your film",""),
+          d = prompt("Rating","");
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
