83a573cec05bd62cf774e0432fc61d0d27d621b4
Chingiz <cingiz99@mail.ru>
2021-04-21 22:49:25 +0400

    Test

--- script.js
+++ script.js
@@ -1,19 +1,78 @@
-const numberOfFilms = +prompt("How much film?");
+// const numberOfFilms = +prompt("How much film?");
 
-    const personalMovieDB = {
-        count:numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };
+//     const personalMovieDB = {
+//         count:numberOfFilms,
+//         movies: {},
+//         actors: {},
+//         genres: [],
+//         privat: false
+//     };
 
-    const a = prompt("Last your film",""),
-          b = prompt("Rating",""),
-          c = prompt("Last your film",""),
-          d = prompt("Rating","");
+//     const a = prompt("Last your film",""),
+//           b = prompt("Rating",""),
+//           c = prompt("Last your film",""),
+//           d = prompt("Rating","");
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+//     personalMovieDB.movies[a] = b;
+//     personalMovieDB.movies[c] = d;
 
-    console.log(personalMovieDB);
+//     console.log(personalMovieDB);
+
+// if(4 == 9){
+//     console.log("OK!");
+// } else {
+//     console.log("Error ");
+// }
+
+// const num = 50;
+
+// if (num<40){
+//     console.log("Error");
+// } else if (num > 100){
+//     console.log("Too much");
+// } else {
+//     console.log("Ok!");
+// }
+
+
+// (num === 50) ? console.log("Ok!") : console.log("Error");
+
+
+// switch (num) {
+//     case 49:
+//         console.log("False");
+//         break;
+//     case 100:
+//         console.log("False");
+//         break;
+//     case 50:
+//         console.log("False");
+//         break;    
+//     default:
+//         console.log("Not today");
+//         break;
+// }
+let num = 50;
+
+// while (num < 55){
+//     console.log(num);
+//     num++;
+// }
+
+// do{
+//     console.log(num);
+//     num++;
+// }
+
+// while (num < 55);
+
+for (let i = 1; i < 10; i++){
+    if (i === 6){
+        break;
+    }
+    console.log(i);
+    i++;
+}
+
+
+
